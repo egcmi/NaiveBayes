c61f2f86de45c033d13bd46bfe70cff0d97bbcc3
Emanuela Joe <Emanuela Joe>
2017-03-22 14:51:40 +0200

    naive bayes spam filter

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,36 +1,62 @@
+# coding: utf-8
 import os
+import numpy
 from collections import Counter
 from sklearn.naive_bayes import GaussianNB
 
-os.chdir('~/Desktop/johtek/NaiveBayes')
+def word_counter(open_file):
+	word_list = []
+	content = open_file.readlines()
+	for i in range(len(content)):
+		for word in content[i].split():
+			if word in dangerous_words:
+				word_list.append(word.lower())
+	count = Counter(dangerous_words)
+	for i in count:
+		count[i] = 0
+	count.update(word_list)
+	return count
 
-dangerous_words = ["click", "here", "the", "and"]
+
+os.chdir('C:\\Users\Emanuela\Documents\\UNI\IntroAI\\NaiveBayes')
+
+dangerous_words = ['click', 'here', 'link', 'cheap', 'buy', 'free', 'viagra',
+				'win', 'cash', 'credit', 'debit', 'card', 'deposit', 'investment',
+				'easy', 'lose', 'weight', 'interest', 'payment', 'money', 'offer',
+				'dollars', 'earnings', 'income', 'junk', 'spam', 'deal', 'financial',
+				'prize', 'casino', 'play', 'extra', 'webcam', 'sexy', 'naked',
+				'secrets', 'easy', 'diet', 'winner', 'rich', '$', 'share']
 
 train = []
 target = []
-for f in os.listdir("./train"):
-	open_file = open("./train/"+f, 'r', newline='\n')
-	spam = open_file.readline()
-	words_list =[]
-	contents = open_file.readlines()
-	for i in range(len(contents)):
-		for word in contents[i].split():
-			if word in dangerous_words:
-				words_list.append(word.lower())
-	open_file.close()
-	count = Counter({'click': 0, 'here': 0, 'the': 0, 'and': 0})
-	count.update(words_list)
-	train.append(np.array(list(count.values())))
-	if spam == "true":
+for f in os.listdir('.\\train'):
+	open_file = open('.\\train\\' + f, 'r', newline='\n', encoding="utf-8")
+	spam = open_file.readline().strip()
+	if spam == 'FALSE':
 		target.append(1)
-	else:
+	elif spam == 'TRUE':
 		target.append(2)
+	else:
+		print('Error: could not classify')
+	word_count = word_counter(open_file)
+	open_file.close()
+	train.append(numpy.array(list(word_count.values())))
 
-#dummy file
-train.append(np.array([3,4,1,2]))
-target.append(2)
 
 gnb = GaussianNB()
 gnb.fit(train, target)
-print(gnb.predict([[3, 4, 2, 5]]))
-
+for f in os.listdir('.\\test'):
+	open_file = open('.\\test\\' + f, 'r', newline='\n', encoding="utf-8")
+	spam = open_file.readline().strip()
+	word_count = word_counter(open_file)
+	open_file.close()
+	pred = gnb.predict([ numpy.array(list(word_count.values())) ])
+	print(spam, pred, end=': ')
+	if spam == 'TRUE' and pred == [2]:
+		print('SPAM WAS CORRECTLY CLASSIFIED')
+	elif spam == 'FALSE' and pred == [1]:
+		print('HAM WAS CORRECTLY CLASSIFIED')
+	elif spam == 'TRUE' and pred == [1]:
+		print('SPAM WAS ERRONEOUSLY CLASSIFIED AS HAM')
+	elif spam == 'FALSE' and pred == [2]:
+		print('HAM WAS ERRONEOUSLY CLASSIFIED AS SPAM')
